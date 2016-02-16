95a892fd1c502a4c51ba50dcc7dff5b3fb1a245e
mel <dyslixec@hotmail.com>
2016-02-16 15:01:54 +1300

    add clear last character click event

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 $(function(){
-/*
-  if C is clicked: clear last pressed button from the display string
-    with substr to remove last character.
-  
-  if a number, operator or decimal is clicked: update text in display.
-    add each clicked button to the end of the display string.
-  
-  if equals is clicked: calculate the total of the display string.
-    replace operator symbol codes with the correct symbols
-      for calculations.
-    use parseFloat to convert numbers in a string into a number.
-*/
 
+  // if a number, operator or decimal is clicked: update text in display.
+  //   add each clicked button to the end of the display string.
+  // if equals is clicked: calculate the total of the display string.
+  //   replace operator symbol codes with the correct symbols
+  //     for calculations.
+  //   use parseFloat to convert numbers in a string into a number.
+
+  $("#display").text(0);
   var entry = "";
 
   // assign click events for all buttons.  
@@ -21,7 +17,7 @@ $(function(){
     if(entry.length > 9){
       e.preventDefault();
     } else {
-      display = $("#display").text(entry);      
+      $("#display").text(entry);      
     }
   });
 
@@ -31,7 +27,16 @@ $(function(){
     $("#display").text(0);
   });
 
-
+  // if C is clicked: clear last pressed button from the display string
+  //   with substr to remove last character.
+  $("#clear").click(function(e){
+    entry = entry.substr(0, entry.length -1);
+    $("#display").text(entry);
+    if(entry.length < 1){
+      $("#display").text(0);
+      e.preventDefault();
+    }
+  });
 
 
 
